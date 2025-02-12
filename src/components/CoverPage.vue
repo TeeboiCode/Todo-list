4265cfc6792f46d392ff70d7dbe12e43efeffc39
temdayoCode <oluwaleyetaiwo5228@gmail.com>
2025-02-12 16:25:48 +0100

    Added the cover page

--- src/components/CoverPage.vue
+++ src/components/CoverPage.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <h2>CoverPageVue</h2>
+  <div class="cover-container">
+    <div class="brand-container">
+      <img
+        src="../assets/logo.png"
+        class="brand animate__animated animate__fadeIn animate__slow"
+        alt=""
+      />
+    </div>
   </div>
 </template>
 
@@ -10,4 +16,29 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.cover-container {
+  width: 100%;
+  height: 100vh;
+  background: #09203e;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.brand-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.brand {
+  width: 200px;
+}
+
+@media (min-width: 768px) {
+  .brand {
+    width: 300px;
+  }
+}
+</style>
--- src/views/HomeApp.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h3>My Home Page</h3>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style></style>
