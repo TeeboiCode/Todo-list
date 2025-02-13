d0631ea844b2cae3072c8d4c813bf4f833f07f0a
temdayoCode <oluwaleyetaiwo5228@gmail.com>
2025-02-13 14:21:25 +0100

    Added 3 components Sign-Up, Login, and AppDashboard

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <router-view />
+  <div>
+    <nav>
+      <router-link to="/sign-up">Sign Up</router-link> |
+      <router-link to="/dashboard">Dashboard</router-link>
+    </nav>
+    <router-view />
+  </div>
 </template>
 
 <style lang="scss">
@@ -8,5 +14,19 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+}
+
+nav {
+  text-align: center;
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
---
+++ src/components/Login and SignUp/Login.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>LoginVue</div>
+</template>
+
+<script>
+export default {
+  name: "LoginVue",
+};
+</script>
+
+<style scoped></style>
