7024417067f0c04016f8cfa9f20172ac717e9a24
temdayoCode <oluwaleyetaiwo5228@gmail.com>
2025-03-08 06:34:19 +0100

    feat: implement remember me functionality for persistent login

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,10 @@
 
       <!-- show AdsProject after 5secs -->
       <div v-else>
-        <AdsProjectVue />
+        <!-- if the local storage data for user remeber me is true show AppDashboard -->
+        <AppDashboardVue v-if="userRememberMe" />
+
+        <AdsProjectVue v-else />
       </div>
     </div>
   </div>
@@ -17,21 +20,39 @@
 <script>
 import CoverPageVue from "../components/CoverPage.vue";
 import AdsProjectVue from "../components/AdsProject.vue";
+import AppDashboardVue from "../components/App/AppDashboard.vue";
 export default {
   name: "HomeView",
   components: {
     CoverPageVue,
     AdsProjectVue,
+    AppDashboardVue,
   },
   data() {
     return {
       showCoverPage: true,
+      userRememberMe: null,
     };
+  },
+  methods: {
+    // get user's data from local storage
+    getUserData() {
+      const userData = JSON.parse(localStorage.getItem("currentUser"));
+      if (userData) {
+        this.userRememberMe = userData.remember_me;
+      } else {
+        return null;
+      }
+
+      console.log(this.userRememberMe);
+    },
   },
   mounted() {
     setTimeout(() => {
       this.showCoverPage = false;
     }, 5000);
+
+    this.getUserData();
   },
 };
 </script>
